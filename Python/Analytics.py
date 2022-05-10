fa61be590c353949c5c613ce3aa2aa61f2020117
konnor-s <65666541+konnor-s@users.noreply.github.com>
2022-05-10 10:53:32 -0500

    Final Python code

--- Python/Analytics.py
+++ Python/Analytics.py
@@ -3,29 +3,23 @@ import matplotlib.pyplot as plt
 import firebase_admin
 from firebase_admin import credentials, firestore, storage, initialize_app
 import numpy as np
-from sklearn.cluster import MeanShift, estimate_bandwidth
-
-x = [1,1,5,6,1,5,10,22,23,23,50,51,51,52,100,112,130,500,512,600,12000,12230]
-
-X = np.array(list(zip(x,np.zeros(len(x)))))
-bandwidth = estimate_bandwidth(X, quantile=.2)
-ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
-ms.fit(X)
-labels = ms.labels_
-cluster_centers = ms.cluster_centers_
-
-labels_unique = np.unique(labels)
-n_clusters_ = len(labels_unique)
-
-for k in range(n_clusters_):
-    my_members = labels == k
-    print("cluster {0}: {1}".format(k, X[my_members, 0]))
 
 
 cred = credentials.Certificate("credentials.json")
 initialize_app(cred, {'storageBucket': 'iotproject-60e4c.appspot.com'})
 
 firestore_db = firestore.client()
+
+exitData = list() #list of times in seconds after midnight
+exitCollection = list(firestore_db.collection('Exit').get()) 
+for doc in exitCollection:
+    timeStr = doc.to_dict()['Time']
+    dt = datetime.datetime.fromisoformat(timeStr)
+    h = dt.hour
+    m = dt.minute
+    s = dt.second
+    t = h*60*60 + m*60 + s
+    exitData.append(t)    
 
 
 identifiedData = list() #list of times in seconds after midnight
@@ -50,22 +44,11 @@ for doc in unidentifiedCollection:
     t = h*60*60 + m*60 + s
     unidentifiedData.append(t)
 
-exitData = list()
-exitCollection = list(firestore_db.collection('Exits').get())
-for doc in exitCollection:
-    timeStr = doc.to_dict()['Time']
-    dt = datetime.datetime.fromisoformat(timeStr)
-    h = dt.hour
-    m = dt.minute
-    s = dt.second
-    t = h*60*60 + m*60 + s
-    exitData.append(t)
-
 onesI = ['entry' for x in identifiedData]
 onesU = ['intrusion' for x in unidentifiedData]
 onesE = ['exit' for x in exitData]
 plt.plot(identifiedData, onesI, '.')
-plt.plot(exitData, onesE)
+plt.plot(exitData, onesE,'.')
 plt.plot(unidentifiedData, onesU, '.')
 
 
@@ -81,7 +64,7 @@ for x,y in zip(unidentifiedData,onesU):
                  ha='center',
                  va='top',
                  rotation=90) # horizontal alignment can be left, right or center
-
+    i += 1
 plt.xlabel('Time of Day')
 
 times = [3600*i for i in range(25)]
--- Python/FacialRecognition.py
+++ Python/FacialRecognition.py
@@ -10,7 +10,8 @@ import imghdr
 from email.message import EmailMessage
 import json
 import datetime
-import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import MeanShift, estimate_bandwidth
 
 emailFile = open('email.json')
 emailData = json.load(emailFile)
@@ -19,14 +20,19 @@ RECIEVER_EMAIL = emailData['Reciever_Email']
 PASSWORD = emailData['Password']
 
 
-known_image1 = face_recognition.load_image_file("KnownImages/biden.jpg")
-known_image2 = face_recognition.load_image_file("KnownImages/biden2.jpg")
-known_image3 = face_recognition.load_image_file("KnownImages/biden4.jpg")
+known_image1 = face_recognition.load_image_file("KnownImages/a1.jpg")
+known_image2 = face_recognition.load_image_file("KnownImages/a2.jpg")
+known_image3 = face_recognition.load_image_file("KnownImages/a3.jpg")
+known_image5 = face_recognition.load_image_file("KnownImages/a5.jpg")
+known_image6 = face_recognition.load_image_file("KnownImages/a6.jpg")
+
 known_encoding1 = face_recognition.face_encodings(known_image1)[0]
 known_encoding2 = face_recognition.face_encodings(known_image2)[0]
 known_encoding3 = face_recognition.face_encodings(known_image3)[0]
+known_encoding5 = face_recognition.face_encodings(known_image5)[0]
+known_encoding6 = face_recognition.face_encodings(known_image6)[0]
 
-known_encodings = [known_encoding1, known_encoding2, known_encoding3]
+known_encodings = [known_encoding1, known_encoding2, known_encoding3, known_encoding5, known_encoding6]
 
 cred = credentials.Certificate("credentials.json")
 initialize_app(cred, {'storageBucket': 'iotproject-60e4c.appspot.com'})
@@ -34,14 +40,68 @@ initialize_app(cred, {'storageBucket': 'iotproject-60e4c.appspot.com'})
 firestore_db = firestore.client()
 bucket = storage.bucket()
 
+exitTimes = list()
 dt = datetime.datetime.now()
-i = dt.minute
+i = 0
 # continuously run this program
 while (1):
-    print(".")
+    
     dt = datetime.datetime.now()
+    dt_h = dt.hour
+    dt_m = dt.minute
+    dt_s = dt.second
+    dt_t = dt_h*60*60 + dt_m*60 + dt_s
 
-   
+    #update exits every minute
+    if (i%12 == 0):
+        exitData = list() #list of times in seconds after midnight 
+        exitCollection = list(firestore_db.collection('Exit').get()) 
+        for doc in exitCollection:
+            timeStr = doc.to_dict()['Time']
+            dt = datetime.datetime.fromisoformat(timeStr)
+            h = dt.hour
+            m = dt.minute
+            s = dt.second
+            t = h*60*60 + m*60 + s
+            exitData.append(t)
+
+        X = np.array(list(zip(exitData,np.zeros(len(exitData)))))
+        bandwidth = estimate_bandwidth(X, quantile=.5)
+        ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+        ms.fit(X)
+        labels = ms.labels_
+        cluster_centers = ms.cluster_centers_
+
+        labels_unique = np.unique(labels)
+        n_clusters_ = len(labels_unique)
+        exitTimes.clear()
+        for k in range(n_clusters_):
+            my_members = labels == k
+            #print("cluster {0}: {1}".format(k, X[my_members, 0]))
+            if len(X[my_members, 0]) > 2:
+                exitTime = np.mean(X[my_members, 0])
+                exitTimes.append(exitTime)
+       
+    #send an email if they will leave in 10 minutes
+    for exitTime in exitTimes:
+        if (dt_t + 605 > exitTime) and (dt_t + 600 < exitTime):
+            newMessage = EmailMessage()
+            newMessage['Subject'] = "Prepare to leave!"
+            newMessage['From'] = SENDER_EMAIL
+            newMessage['To'] = RECIEVER_EMAIL
+            newMessage.set_content("Don't forget to shut off the lights!\nOr I'll shut off yours...")
+
+            with open('robot.jpg', 'rb') as f:
+                image_data = f.read()
+                image_type = imghdr.what(f.name)
+                image_name = f.name
+
+            newMessage.add_attachment(image_data, maintype='image',
+                                    subtype=image_type, filename=image_name)
+
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+                smtp.login(SENDER_EMAIL, PASSWORD)
+                smtp.send_message(newMessage)
             
 
     uncheckedImages = list(bucket.list_blobs(prefix="Unchecked/"))
@@ -82,22 +142,23 @@ while (1):
             else:  
                 print("Intruder Alert!")
                 #upload to storage
-                imageToUpload = bucket.blob("Intruders/" + imageName)
-                imageToUpload.upload_from_filename("Unchecked/" + imageName)
+
+                imageToUpload = bucket.blob("Intruders/" + str(dt))
+                imageToUpload.upload_from_filename("Unchecked/image.png")
 
                 firestore_db.collection('Unidentified').document(dt_str).set({
                     'Time':dt_str})
 
                 newMessage = EmailMessage()
-                newMessage['Subject'] = "Intruder Detected: " + imageName.split(".")[0]
+                newMessage['Subject'] = "Intruder Detected!"
                 newMessage['From'] = SENDER_EMAIL
                 newMessage['To'] = RECIEVER_EMAIL
-                newMessage.set_content('Image attached')
+                newMessage.set_content("Intruder Detected at " + str(dt))
 
                 with open('Unchecked/' + imageName, 'rb') as f:
                     image_data = f.read()
                     image_type = imghdr.what(f.name)
-                    image_name = f.name
+                    image_name = "intruder"
 
                 newMessage.add_attachment(image_data, maintype='image',
                                         subtype=image_type, filename=image_name)
@@ -112,3 +173,4 @@ while (1):
         os.remove("Unchecked/" + imageName)
 
     sleep(5)
+    i += 1
